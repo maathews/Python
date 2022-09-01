218efb7fe6636c90946783d506714307969da27e
Mandaloriano <matheus.vision@live.com>
2022-08-31 21:58:49 -0300

    Create python#16.py

---
+++ Exercicios/python#16.py
@@ -0,0 +1,7 @@
+import math
+inteiro=float(input('Digite o seu valor: '))
+print('O numero digitado foi {} a parte inteira é {}'.format(inteiro,math.trunc(inteiro)))
+
+from math import trunc
+inteiro=float(input('Digite o seu valor: '))
+print('O numero digitado foi {} a parte inteira é {}'.format(inteiro,trunc(inteiro)))
